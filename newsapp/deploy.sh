bd6f719f61944127f1479bbe63cc3368e27c0515
Sanil Khurana <sanilkhurana7@gmail.com>
2020-05-30 20:48:55 +0530

    Added EC2 deployment script

--- newsapp/deploy.sh
+++ newsapp/deploy.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
+# For deployment on S3
 yarn build
 ACCESS_KEY=$(cat mainInstanceRootKey.csv | grep -P "AWSAccessKeyId=[A-Za-z0-9]+" | cut -d "=" -f2)
 SECRET_KEY=$(cat mainInstanceRootKey.csv | grep -P "AWSSecretKey=[A-Za-z0-9]+" | cut -d "=" -f2)
 aws s3 sync build/* s3://react-newsapp
+
+# For deployment on EC2
+# yarn build
+# # TODO: Some sed/awk command here to replace static with react-newsapp/static
+# scp -r -i ~/Documents/aws/main-instance.pem build/* ubuntu@3.7.175.185:~/react-newsapp/
